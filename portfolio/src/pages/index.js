44c069142307685fe46e95f11e3c043f9bb6f34e
cassidy <cassidy.nguyen@epitech.eu>
2024-06-28 09:57:40 +0200

    push fix

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
             </div>
             <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
-                text="Développeur web à la recherche d&apos;'une alternance"
+                text="Développeur web à la recherche d&apos;une alternance"
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl
                  md:!text-5xl sm:!text-3xl"
               />
